5b6ef3d7de58e8058005fd63d4ae9cb790e2083a
dbaezh78 <108948897+dbaezh78@users.noreply.github.com>
2025-03-12 17:26:30 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const contenido = document.getElementById("contenido");
+    contenido.innerHTML = `
+        <p>1 Este contenido ha sido cargado desde un archivo JavaScript.</p>
+    `;
+});
